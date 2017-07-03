0e1d3708ac35f9e71a43d9eca5e56221b021011d
John Tolentino <jpftolentino@gmail.com>
2017-07-03 22:18:19 +0000

    Step 7: Downloading github avatars using url and file path as arguments

--- download_avatar.js
+++ download_avatar.js
@@ -1,18 +1,50 @@
 var request = require('request');
+var fs = require('fs');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+var GITHUB_USER = 'jpftolentino';
+var GITHUB_TOKEN = '1cc7286ae8e54f8294fc4c6ea9f900a1a447d79c';
+
 
 function getRepoContributors(repoOwner, repoName, cb) {
 
   var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
   console.log(requestURL);
+
+  //must create options variable from github documents to access the database
+  var option = {
+    url: requestURL,
+    headers: {
+      'User-Agent': 'github-avatar-downloader'
+    }
+  };
+
+  //must pass option object with the proper keys in order to make  a successful request
+    request(option, function(err, response, body) {
+      if (err) {
+        console.log(`Error fetching: ${option}`, err);
+        return;
+      }
+
+    if(response.statusCode === 200) {
+      var json = JSON.parse(body);
+      json.forEach(function(item) {
+        console.log(item["avatar_url"]);
+      })
+
+    }
+  });
 }
 
-var GITHUB_USER = 'jpftolentino';
-var GITHUB_TOKEN = '1cc7286ae8e54f8294fc4c6ea9f900a1a447d79c';
+function downloadImageByURL(url, filePath) {
+  request(url).pipe(fs.createWriteStream("./",filePath));
+}
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
 });
 
+// downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg");
+downloadImageByURL();
