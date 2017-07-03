446752348be2aaa35236342ef089a7b0b727f361
John Tolentino <jpftolentino@gmail.com>
2017-07-03 19:06:39 +0000

    Step 4 of project completed

--- download_avatar.js
+++ download_avatar.js
@@ -3,10 +3,16 @@ var request = require('request');
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  console.log(requestURL);
 }
+
+var GITHUB_USER = 'jpftolentino';
+var GITHUB_TOKEN = '1cc7286ae8e54f8294fc4c6ea9f900a1a447d79c';
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
 });
+
